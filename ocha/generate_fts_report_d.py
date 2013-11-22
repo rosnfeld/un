efb87cba4e6bf22e6ce7f35638a9c0faefcdaccd
Andrew Rosenfeld <rosnfeld@gmail.com>
2013-11-22 13:15:44 -0500

    Most of the way done on a type "D" report

---
+++ ocha/generate_fts_report_d.py
@@ -0,0 +1,32 @@
+"""
+Take in an appeal ID and generate a type "D" FTS report:
+Cluster
+Original Requirements USD
+Revised Requirements USD
+Carry Over USD - this is currently not broken out, though could be
+Funding USD - this is the funding (contributions + commitments) separate from carry over, also not currently broken out
+Total Resources Available USD
+Unmet Requirements USD
+% Covered
+Uncommitted pledges USD
+"""
+
+import fts_queries
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Produce a FTS type "D" report for a given appeal')
+parser.add_argument('appeal_id', type=int)
+args = parser.parse_args()
+appeal_id = args.appeal_id
+
+cluster_data = fts_queries.fetch_clusters_json_for_appeal_as_dataframe(appeal_id)
+
+cluster_data['unmet_requirement'] = cluster_data.current_requirement - cluster_data.funding
+# note this is a fraction, not a percent, and not clipped to 100%
+cluster_data['fraction_covered'] = cluster_data.funding/cluster_data.current_requirement
+
+#TODO reorder the columns
+
+#For quick debugging just do: print cluster_data
+cluster_data.to_csv(sys.stdout, index=False, encoding='utf-8')
