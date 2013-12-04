84ce86e83a2ccee9bcd59ac7225d7c7dbde8cd02
Andrew Rosenfeld <rosnfeld@gmail.com>
2013-12-04 14:14:20 -0500

    Pulling CSV-writing into one place and logging progress

--- ocha/dap/fts/ckan_loading/produce_csvs.py
+++ ocha/dap/fts/ckan_loading/produce_csvs.py
@@ -11,28 +11,33 @@ Can be used to produce the following CSV files for upload into CKAN:
 
 import fts_queries
 
-
-# TODO progress logging would be nice, e.g. "Writing foo.csv"
 # TODO extract strings to header section
 # TODO should all CSV's have "fts" prefix, e.g. 'fts_sectors.csv'?
-# TODO should we build subdirs for staging
+# TODO and/or should we build subdirs for staging
 #  - maybe try this structure: <output_dir>/fts/global/..., <output_dir>/fts/per_country/<country>/...
 #  - I think that maybe makes more sense than namespacing in filename... or could do both?
 
 
+def write_dataframe_to_csv(dataframe, path):
+    print "Writing", path
+    # include the index which is an ID for each of the objects serialized by this script
+    # use Unicode as many non-ASCII characters
+    dataframe.to_csv(path, index=True, encoding='utf-8')
+
+
 def produce_sectors_csv(output_dir):
     sectors = fts_queries.fetch_sectors_json_as_dataframe()
-    sectors.to_csv(output_dir + '/sectors.csv', index=True, encoding='utf-8')
+    write_dataframe_to_csv(sectors, output_dir + 'sectors.csv')
 
 
 def produce_countries_csv(output_dir):
     countries = fts_queries.fetch_countries_json_as_dataframe()
-    countries.to_csv(output_dir + '/countries.csv', index=True, encoding='utf-8')
+    write_dataframe_to_csv(countries, output_dir + 'countries.csv')
 
 
 def produce_organizations_csv(output_dir):
     organizations = fts_queries.fetch_organizations_json_as_dataframe()
-    organizations.to_csv(output_dir + '/organizations.csv', index=True, encoding='utf-8')
+    write_dataframe_to_csv(organizations, output_dir + 'organizations.csv')
 
 
 def produce_global_csvs(output_dir):
