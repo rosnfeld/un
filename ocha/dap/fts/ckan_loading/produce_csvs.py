d9f1c1eaead63080952a691e22985a2d21757e54
Andrew Rosenfeld <rosnfeld@gmail.com>
2013-12-04 14:44:48 -0500

    Creating subdirectories for each type of data

--- ocha/dap/fts/ckan_loading/produce_csvs.py
+++ ocha/dap/fts/ckan_loading/produce_csvs.py
@@ -10,12 +10,10 @@ Can be used to produce the following CSV files for upload into CKAN:
 """
 
 import fts_queries
+import os
 
 # TODO extract strings to header section
-# TODO should all CSV's have "fts" prefix, e.g. 'fts_sectors.csv'?
-# TODO and/or should we build subdirs for staging
-#  - maybe try this structure: <output_dir>/fts/global/..., <output_dir>/fts/per_country/<country>/...
-#  - I think that maybe makes more sense than namespacing in filename... or could do both?
+# TODO should all CSV's have "fts" prefix, e.g. 'fts_sectors.csv', and maybe ISO country code if appropriate?
 
 
 def write_dataframe_to_csv(dataframe, path):
@@ -40,25 +38,38 @@ def produce_organizations_csv(output_dir):
     write_dataframe_to_csv(organizations, output_dir + 'organizations.csv')
 
 
-def produce_global_csvs(output_dir):
+def produce_global_csvs(base_output_dir):
+    # not sure if this directory creation code should be somewhere else..?
+    output_dir = base_output_dir + '/fts/global/'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
     produce_sectors_csv(output_dir)
     produce_countries_csv(output_dir)
     produce_organizations_csv(output_dir)
 
 
-# def produce_emergencies_csv_for_country(output_dir, country):
-#     emergencies = fts_queries.fetch_emergencies_json_for_country_as_dataframe(country)
-#     emergencies.to_csv(output_dir + '/emergencies.csv', index=True, encoding='utf-8')
-#
-# def produce_csvs_for_country(output_dir, country):
-#     # TODO modify output_dir here?
-#     produce_emergencies_csv_for_country(output_dir, country)
+def produce_emergencies_csv_for_country(output_dir, country):
+    emergencies = fts_queries.fetch_emergencies_json_for_country_as_dataframe(country)
+    write_dataframe_to_csv(emergencies, output_dir + 'emergencies.csv')
 
+
+def produce_csvs_for_country(base_output_dir, country):
+    output_dir = base_output_dir + '/fts/per_country/' + country + '/'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    produce_emergencies_csv_for_country(output_dir, country)
+
+# TODO appeals
+# TODO projects
+# TODO contributions
 
 if __name__ == "__main__":
     # output all CSVs for a given country to '/tmp/'
-    country = 'Chad'
-    output_dir = '/tmp/'
+    demo_country = 'Chad'
+    tmp_output_dir = '/tmp'
 
-    produce_global_csvs(output_dir)
+    produce_global_csvs(tmp_output_dir)
+    produce_csvs_for_country(tmp_output_dir, demo_country)
 
