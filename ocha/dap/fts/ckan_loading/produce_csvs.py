afb03159b1d995572571a168c44999db7dcb3a58
Andrew Rosenfeld <rosnfeld@gmail.com>
2013-12-08 15:01:54 -0500

    Output CSVs for 4 starter countries; CSV filenames now include country name

--- ocha/dap/fts/ckan_loading/produce_csvs.py
+++ ocha/dap/fts/ckan_loading/produce_csvs.py
@@ -14,7 +14,19 @@ import os
 import pandas as pd
 
 # TODO extract strings to header section above the code
-# TODO should all CSV's have "fts" prefix, e.g. 'fts_sectors.csv', and maybe ISO country code if appropriate?
+
+
+def build_csv_path(base_path, object_type, country=None):
+    """
+    Using CSV names that duplicate the file paths here, which generally I don't like,
+    but having very explicit filenames is maybe nicer to sort out for CKAN.
+    """
+    filename = 'fts_' + object_type + '.csv'
+
+    if country:  # a little bit of duplication but easier to read
+        filename = 'fts_' + country + '_' + object_type + '.csv'
+
+    return os.path.join(base_path, filename)
 
 
 def write_dataframe_to_csv(dataframe, path):
@@ -31,22 +43,22 @@ def filter_out_empty_dataframes(dataframes):
 
 def produce_sectors_csv(output_dir):
     sectors = fts_queries.fetch_sectors_json_as_dataframe()
-    write_dataframe_to_csv(sectors, output_dir + 'sectors.csv')
+    write_dataframe_to_csv(sectors, build_csv_path(output_dir, 'sectors'))
 
 
 def produce_countries_csv(output_dir):
     countries = fts_queries.fetch_countries_json_as_dataframe()
-    write_dataframe_to_csv(countries, output_dir + 'countries.csv')
+    write_dataframe_to_csv(countries, build_csv_path(output_dir, 'countries'))
 
 
 def produce_organizations_csv(output_dir):
     organizations = fts_queries.fetch_organizations_json_as_dataframe()
-    write_dataframe_to_csv(organizations, output_dir + 'organizations.csv')
+    write_dataframe_to_csv(organizations, build_csv_path(output_dir, 'organizations'))
 
 
 def produce_global_csvs(base_output_dir):
     # not sure if this directory creation code should be somewhere else..?
-    output_dir = base_output_dir + '/fts/global/'
+    output_dir = os.path.join(base_output_dir, 'fts', 'global')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -57,12 +69,12 @@ def produce_global_csvs(base_output_dir):
 
 def produce_emergencies_csv_for_country(output_dir, country):
     emergencies = fts_queries.fetch_emergencies_json_for_country_as_dataframe(country)
-    write_dataframe_to_csv(emergencies, output_dir + 'emergencies.csv')
+    write_dataframe_to_csv(emergencies, build_csv_path(output_dir, 'emergencies', country=country))
 
 
 def produce_appeals_csv_for_country(output_dir, country):
     appeals = fts_queries.fetch_appeals_json_for_country_as_dataframe(country)
-    write_dataframe_to_csv(appeals, output_dir + 'appeals.csv')
+    write_dataframe_to_csv(appeals, build_csv_path(output_dir, 'appeals', country=country))
 
 
 def produce_projects_csv_for_country(output_dir, country):
@@ -73,7 +85,7 @@ def produce_projects_csv_for_country(output_dir, country):
     list_of_projects = [fts_queries.fetch_projects_json_for_appeal_as_dataframe(appeal_id) for appeal_id in appeal_ids]
     list_of_non_empty_projects = filter_out_empty_dataframes(list_of_projects)
     projects_frame = pd.concat(list_of_non_empty_projects)
-    write_dataframe_to_csv(projects_frame, output_dir + 'projects.csv')
+    write_dataframe_to_csv(projects_frame, build_csv_path(output_dir, 'projects', country=country))
 
 
 def produce_contributions_csv_for_country(output_dir, country):
@@ -85,11 +97,11 @@ def produce_contributions_csv_for_country(output_dir, country):
                              for emergency_id in emergency_ids]
     list_of_non_empty_contributions = filter_out_empty_dataframes(list_of_contributions)
     contributions_master_frame = pd.concat(list_of_non_empty_contributions)
-    write_dataframe_to_csv(contributions_master_frame, output_dir + 'contributions.csv')
+    write_dataframe_to_csv(contributions_master_frame, build_csv_path(output_dir, 'contributions', country=country))
 
 
 def produce_csvs_for_country(base_output_dir, country):
-    output_dir = base_output_dir + '/fts/per_country/' + country + '/'
+    output_dir = os.path.join(base_output_dir, 'fts', 'per_country', country)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -100,10 +112,11 @@ def produce_csvs_for_country(base_output_dir, country):
 
 
 if __name__ == "__main__":
-    # output all CSVs for a given country to '/tmp/'
-    demo_country = 'Chad'
-    tmp_output_dir = '/tmp'
+    # output all CSVs for the given countries to '/tmp/'
+    #country_codes = ['COL', 'SSD', 'YEM', 'PAK']  # the set of starter countries for DAP
+    country_codes = ['PAK']  # the set of starter countries for DAP
+    tmp_output_dir = '/tmp/'
 
     produce_global_csvs(tmp_output_dir)
-    produce_csvs_for_country(tmp_output_dir, demo_country)
-
+    for country_code in country_codes:
+        produce_csvs_for_country(tmp_output_dir, country_code)
