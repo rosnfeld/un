a76462d6fe2860b97078fccbd7392939bf4f0f6e
Andrew Rosenfeld <rosnfeld@gmail.com>
2013-12-13 21:36:31 -0500

    Widening set of percentage columns included, but also excluding some known exceptions

--- ocha/dap/validation/scraperwiki.py
+++ ocha/dap/validation/scraperwiki.py
@@ -4,7 +4,7 @@ Just some initial poking around with ScraperWiki data
 
 import pandas as pd
 
-BASE_DIR = '/home/andrew/un/ocha/dap/'
+BASE_DIR = '/home/andrew/un/ocha/dap/scraperwiki_2013-12-13/'
 DATASET_CSV = BASE_DIR + 'dataset.csv'
 INDICATOR_CSV = BASE_DIR + 'indicator.csv'
 VALUE_CSV = BASE_DIR + 'value.csv'
@@ -22,9 +22,9 @@ def get_value_frame():
     return pd.read_csv(VALUE_CSV)
 
 
-def check_percentages_between_0_and_100():
+def get_percentage_indicators():
     """
-    Read all indicators that are expressed in percentage terms and confirm values lie between 0% and 100%.
+    Returns a set of indicator IDs that have percentage units
     """
     all_indicators = get_indicator_frame()
 
@@ -34,23 +34,46 @@ def check_percentages_between_0_and_100():
         if not isinstance(unit_string, basestring):
             return False
 
-        if unit_string.startswith('%'):
+        if '%' in unit_string:
             return True
 
-        if unit_string.startswith('percentage'):
+        if 'percentage' in unit_string:
             return True
 
         return False
 
     percentage_indicators = all_indicators[all_indicators.units.apply(is_percentage_unit)]
+    return set(percentage_indicators.index)
 
+
+def find_percentage_values_outside_0_to_100():
+    """
+    Read all indicators that are expressed in percentage terms and confirm values lie between 0% and 100%.
+    Yes, many indicators may naturally not be in this range, but this is just
+    """
     all_values = get_value_frame()
-    percentage_values = all_values[all_values.indID.apply(lambda x: x in percentage_indicators.index)]
+    percentage_indicators = get_percentage_indicators()
+
+    # some percentages are okay to be outside the range:
+    # PSP050, PSP060 - population growth rate
+    # PSE150 - age dependency ratio
+    # PSE200 - consumer price inflation
+    excluded_indicators = {'PSP050', 'PSP060', 'PSE150', 'PSE200'}
+
+    percentage_indicators = percentage_indicators - excluded_indicators
+
+    percentage_values = all_values[all_values.indID.apply(lambda x: x in percentage_indicators)]
 
     # need to convert value column, as for some indicators it can be string but for these it should be numeric
+    # TODO use "is_number" column for this?
     percentage_values = percentage_values.copy()
     percentage_values.value = percentage_values.value.astype(float)
 
     values_outside_range = percentage_values[(percentage_values.value < 0) | (percentage_values.value > 100)]
 
     return values_outside_range
+
+
+if __name__ == "__main__":
+    print "Percentage Values outside 0% to 100%:"
+    print find_percentage_values_outside_0_to_100()
