5ce9c799c39e4731929b0ffa4dd90a65a6996b09
Andrew Rosenfeld <rosnfeld@gmail.com>
2013-12-16 22:51:40 -0500

    Adding test for negative values of count-based indicators

--- ocha/dap/validation/scraperwiki.py
+++ ocha/dap/validation/scraperwiki.py
@@ -46,10 +46,34 @@ def get_percentage_indicators():
     return set(percentage_indicators.index)
 
 
+def get_incidence_indicators():
+    """
+    Returns a set of indicator IDs that are measured by some sort of "count"
+    (e.g. #people affected by disasters)
+    """
+    all_indicators = get_indicator_frame()
+
+    def is_incidence_unit(unit_string):
+        # TODO could definitely be improved
+
+        if not isinstance(unit_string, basestring):
+            return False
+
+        if unit_string in ('uno', 'count', 'thousands', 'millions'):
+            return True
+
+        if ' per ' in unit_string:  # one could argue this is its own category
+            return True
+
+        return False
+
+    incidence_indicators = all_indicators[all_indicators.units.apply(is_incidence_unit)]
+    return set(incidence_indicators.index)
+
+
 def find_percentage_values_outside_0_to_100():
     """
     Read all indicators that are expressed in percentage terms and confirm values lie between 0% and 100%.
-    Yes, many indicators may naturally not be in this range, but this is just
     """
     all_values = get_value_frame()
     percentage_indicators = get_percentage_indicators()
@@ -74,6 +98,32 @@ def find_percentage_values_outside_0_to_100():
     return values_outside_range
 
 
+# TODO refactor duplication with previous method
+def find_negative_incidence_values():
+    """
+    Read all indicators that are expressed in incidence terms and confirm values are not negative.
+    """
+    all_values = get_value_frame()
+    incidence_indicators = get_incidence_indicators()
+
+    incidence_values = all_values[all_values.indID.apply(lambda x: x in incidence_indicators)]
+
+    # need to convert value column, as for some indicators it can be string but for these it should be numeric
+    # TODO use "is_number" column for this?
+    incidence_values = incidence_values.copy()
+    incidence_values.value = incidence_values.value.astype(float)
+
+    values_outside_range = incidence_values[incidence_values.value < 0]
+
+    return values_outside_range
+
+
 if __name__ == "__main__":
-    print "Percentage Values outside 0% to 100%:"
-    print find_percentage_values_outside_0_to_100()
+    # print "Percentage Values outside 0% to 100%:"
+    # print find_percentage_values_outside_0_to_100()
+
+    print "Incidence Values below 0:"
+    print find_negative_incidence_values()
+
+
+# TODO restructure this to print which indicators are being tested, with what criteria
