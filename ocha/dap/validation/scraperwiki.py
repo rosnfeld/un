839df8987370f3315a88241a56afcd5100a570ab
Andrew Rosenfeld <rosnfeld@gmail.com>
2013-12-16 23:08:21 -0500

    Refactoring to remove duplication

--- ocha/dap/validation/scraperwiki.py
+++ ocha/dap/validation/scraperwiki.py
@@ -60,62 +60,68 @@ def is_incidence_unit(unit_string):
 
 
 def get_indicators_matching_units(criteria_function):
+    """
+    Return the set of indicator IDs that match a given criteria function on the indicator units
+    """
     all_indicators = get_indicator_frame()
     matching_indicators = all_indicators[all_indicators.units.apply(criteria_function)]
     return set(matching_indicators.index)
+
+
+def find_values_out_of_bounds(units_criteria_function, excluded_indicators=None, lower_bound=None, upper_bound=None):
+    """
+    Return all indicators that match the criteria function (but allow for manual exclusions),
+    and return those that are outside the specified bounds
+    """
+    all_values = get_value_frame()
+    indicators = get_indicators_matching_units(units_criteria_function)
+
+    if excluded_indicators:
+        indicators -= excluded_indicators
+
+    indicator_values = all_values[all_values.indID.apply(lambda x: x in indicators)]
+
+    # need to convert value column, as for some indicators it can be string but for these it should be numeric
+    # TODO use "is_number" column for this?
+    indicator_values = indicator_values.copy()
+    indicator_values.value = indicator_values.value.astype(float)
+
+    violations_series = pd.Series(False, index=indicator_values.index)
+
+    if lower_bound:
+        violations_series |= indicator_values.value < lower_bound
+
+    if upper_bound:
+        violations_series |= indicator_values.value > upper_bound
+
+    return indicator_values[violations_series]
 
 
 def find_percentage_values_outside_0_to_100():
     """
     Read all indicators that are expressed in percentage terms and confirm values lie between 0% and 100%.
     """
-    all_values = get_value_frame()
-    percentage_indicators = get_indicators_matching_units(is_percentage_unit)
-
     # some percentages are okay to be outside the range:
     # PSP050, PSP060 - population growth rate
     # PSE150 - age dependency ratio
     # PSE200 - consumer price inflation
     excluded_indicators = {'PSP050', 'PSP060', 'PSE150', 'PSE200'}
 
-    percentage_indicators = percentage_indicators - excluded_indicators
-
-    percentage_values = all_values[all_values.indID.apply(lambda x: x in percentage_indicators)]
-
-    # need to convert value column, as for some indicators it can be string but for these it should be numeric
-    # TODO use "is_number" column for this?
-    percentage_values = percentage_values.copy()
-    percentage_values.value = percentage_values.value.astype(float)
-
-    values_outside_range = percentage_values[(percentage_values.value < 0) | (percentage_values.value > 100)]
-
-    return values_outside_range
+    return find_values_out_of_bounds(is_percentage_unit, excluded_indicators, lower_bound=0, upper_bound=100)
 
 
-# TODO refactor duplication with previous method
 def find_negative_incidence_values():
     """
     Read all indicators that are expressed in incidence terms and confirm values are not negative.
     """
-    all_values = get_value_frame()
-    incidence_indicators = get_indicators_matching_units(is_incidence_unit)
 
-    incidence_values = all_values[all_values.indID.apply(lambda x: x in incidence_indicators)]
-
-    # need to convert value column, as for some indicators it can be string but for these it should be numeric
-    # TODO use "is_number" column for this?
-    incidence_values = incidence_values.copy()
-    incidence_values.value = incidence_values.value.astype(float)
-
-    values_outside_range = incidence_values[incidence_values.value < 0]
-
-    return values_outside_range
+    return find_values_out_of_bounds(is_incidence_unit, lower_bound=0)
 
 
 if __name__ == "__main__":
     print "Percentage Values outside 0% to 100%:"
     print find_percentage_values_outside_0_to_100()
-
+    print
     print "Incidence Values below 0:"
     print find_negative_incidence_values()
 
