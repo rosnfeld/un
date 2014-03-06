23ddf00146889b818bd65332df193e3f9352184e
Andrew Rosenfeld <rosnfeld@gmail.com>
2014-03-06 12:23:02 -0500

    Adding neighbor lists, analysis start date

--- ocha/dap/validation/deep_dive.py
+++ ocha/dap/validation/deep_dive.py
@@ -10,24 +10,44 @@ Things to do:
 import scraperwiki
 import os
 import matplotlib.pyplot as plt
+import datetime
 
 REGIONS_OF_INTEREST = ['COL', 'KEN']
+NEIGHBORS = {
+    'COL': ['BRA', 'ECU', 'PAN', 'PER', 'VEN'],
+    'KEN': ['ETH', 'SDN', 'SOM', 'SSD', 'TZA', 'UGA']
+}
+
+ANALYSIS_START_DATE = datetime.date(1990, 1, 1)
+
+
+def build_analysis_matrix(region):
+    joined = scraperwiki.get_joined_frame()
+    numeric = scraperwiki.get_numeric_version(joined)
+
+    regions = [region] + NEIGHBORS[region]
+    filtered = numeric[numeric.region.apply(lambda x: x in regions)]
+
+    scraperwiki.add_standardized_period(filtered)
+    filtered = filtered[filtered.period_end > ANALYSIS_START_DATE]
+
+    return filtered
 
 
 if __name__ == '__main__':
     # save off indicator series plots for all numeric indicators for regions of interest
-    joined = scraperwiki.get_joined_frame()
-    numeric = scraperwiki.get_numeric_version(joined)
 
     for region in REGIONS_OF_INTEREST:
+        dataframe = build_analysis_matrix(region)
+
         dir_path = os.path.join('/tmp/deep_dive', region)
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
-        for i, row in numeric[['indID', 'dsID']].drop_duplicates().iterrows():
+        for i, row in dataframe[['indID', 'dsID']].drop_duplicates().iterrows():
             ind_id = row['indID']
             ds_id = row['dsID']
-            figure = scraperwiki.plot_indicator_timeseries_for_region(numeric, ind_id, ds_id, region)
+            figure = scraperwiki.plot_indicator_timeseries_for_region(dataframe, ind_id, ds_id, region)
 
             if not figure:
                 continue
