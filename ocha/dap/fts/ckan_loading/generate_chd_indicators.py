911aae42b58d5579f63e5d6397749d9cf4290c19
Andrew Rosenfeld <rosnfeld@gmail.com>
2014-04-10 13:20:04 -0400

    Adding organization-type funding indicators

--- ocha/dap/fts/ckan_loading/generate_chd_indicators.py
+++ ocha/dap/fts/ckan_loading/generate_chd_indicators.py
@@ -1,5 +1,7 @@
 """
 Builds CHD indicators from FTS queries
+
+TODO consider zeros vs missing data for these indicators
 """
 
 import fts_queries
@@ -51,6 +53,48 @@ def populate_appeals_level_data(country):
         add_row_to_values('FY030', country, year, row['funding'] / row['current_requirements'])
 
 
+def populate_organization_level_data(country):
+    # this call is slow... possibly extract/cache it?
+    organizations = fts_queries.fetch_organizations_json_as_dataframe()
+    # we'll be joining on name (sadly) so make that the index
+    organizations = organizations.set_index('name')
+
+    # load appeals, analyze each one
+    appeals = fts_queries.fetch_appeals_json_for_country_as_dataframe(country)
+
+    funding_dataframes_by_appeal = []
+
+    for appeal_id, appeal_row in appeals.iterrows():
+        # first check if there is any funding at all (otherwise API calls will get upset)
+        if appeal_row['funding'] == 0:
+            continue
+
+        # query funding by recipient, including "carry over" from previous years
+        funding_by_recipient = fts_queries.fetch_funding_json_for_appeal_as_dataframe(
+            appeal_id, grouping='Recipient', alias='organisation')
+
+        funding_by_recipient['year'] = appeal_row['year']
+
+        funding_dataframes_by_appeal.append(funding_by_recipient)
+
+    funding_by_recipient_overall = pd.concat(funding_dataframes_by_appeal)
+
+    # now roll up by organization type
+    funding_by_type = funding_by_recipient_overall.join(organizations.type).groupby(['type', 'year']).sum()
+
+    for (org_type, year), row in funding_by_type.iterrows():
+        if org_type == 'NGOs':
+            add_row_to_values('FY190', country, year, row['funding'])
+        elif org_type == 'Private Orgs. & Foundations':
+            add_row_to_values('FY200', country, year, row['funding'])
+        elif org_type == 'UN Agencies':
+            add_row_to_values('FY210', country, year, row['funding'])
+        else:
+            # note that some organizations (e.g. Red Cross/Red Crescent) fall outside the 3 indicator categories
+            print 'Ignoring funding for organization type ' + org_type
+
+
 if __name__ == "__main__":
     populate_appeals_level_data('KEN')
+    populate_organization_level_data('KEN')
     print get_values_as_dataframe()
